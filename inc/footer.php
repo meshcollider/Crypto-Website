9bde4db76e14be48914786ceac24164ffe68ba48
MeshCollider <dobsonsa68@gmail.com>
2015-06-04 12:29:44 +1200

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,16 @@
+<?php
+function get_footer() {
+?>
+		<div class="footer hidden-xs">
+			<div class="container">
+				<p class="text-muted" style="margin:10px 0;">
+					&copy; Crypto 2015
+					<span class="pull-right">Server Time: <?php echo date("Y-m-d H:i:s"); ?></span>
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
+<?php
+}
+?>
